bdda0fe1883b6b7d8d8fbe9e160a9ea3145c15d2
Ross Mitchell <ross.mitchell@gmail.com>
2017-07-13 16:37:58 +0000

    Adding some basic config files and the Test folders

---
+++ .travis/run-scripts.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+
+if [[ ${GENERATE_COVERAGE} == "1" ]]
+then
+    cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/integration
+    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-php=${TRAVIS_BUILD_DIR}/build/cov/clover-integration.cov
+    cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/unit
+    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-php=${TRAVIS_BUILD_DIR}/build/cov/clover-unit.cov
+    php ${TRAVIS_BUILD_DIR}/magento2/vendor/bin/phpcov merge --clover ${TRAVIS_BUILD_DIR}/build/logs/clover.xml ${TRAVIS_BUILD_DIR}/build/cov
+else
+    cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/${TEST_TYPE}
+    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml
+fi
